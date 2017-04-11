5cb1d57c8c486e274f1e7b6170d8f0621a744287
maurolepore <maurolepore@gmail.com>
2017-04-11 13:42:44 -0500

    rebuild pkg and sige

--- data-raw/src2doc_make_step2.R
+++ data-raw/src2doc_make_step2.R
@@ -1,10 +1,13 @@
 # STEP 2: AFTER src2doc_make_step1.R do this:
 
 # - document and export functions via NAMESPACE, e.g. by clicking Build & Reload;
-
 # - Ammend namespace for functions to export as functions, not as S3 methods;
 source("./data-raw/src2doc_namespace_amend.R")
+# - check package (if necessary, remove errors and restart this process);
+# - build package (click build source package);
+# - build site (click Addins build pkgdown);
+# - push;
+# - try install branch;
+# - merge with master and push;
+# - try install.
 
-# - check package and remove errors and minimize warnings and notes;
-
-# - build package.
