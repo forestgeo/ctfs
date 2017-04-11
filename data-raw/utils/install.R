6cffebefe1f59e23b501818d760ca943a09f0562
Mauro Lepore <maurolepore@users.noreply.github.com>
2017-04-11 15:36:01 -0400

    Delete install.R

--- data-raw/utils/install.R
+++
@@ -1,4 +0,0 @@
-tk <- "58c3725a83a8c53f8a8883bfd747442867279173"
-# devtools::install_github("forestgeo/ctfs@dev", auth_token = tk)
-devtools::install_github("forestgeo/ctfs", auth_token = tk)
-library(ctfs)
