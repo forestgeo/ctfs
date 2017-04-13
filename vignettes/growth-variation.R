ca67ea4acc46d698af95b4b01792da4f64ab6a55
maurolepore <maurolepore@gmail.com>
2017-04-12 23:23:21 -0500

    Vignette runs with library(bci)

---
+++ vignettes/growth-variation.R
@@ -0,0 +1,19 @@
+## ---- echo = FALSE, message=FALSE, warning=FALSE-------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  echo = TRUE
+)
+
+## ---- message=FALSE------------------------------------------------------
+# installation instructions at https://github.com/forestgeo/ctfs
+library(ctfs)
+# installation instructions at https://github.com/lme4/lme4/
+library(lme4)
+# installation instructions at https://forestgeo.github.io/bci/
+library(bci)
+
+## ---- message=FALSE------------------------------------------------------
+# install.packages("tidyverse")
+library(tidyverse)
+
