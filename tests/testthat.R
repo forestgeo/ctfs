a042cec3aec15e9c5eb00986dffdffced4c4f3d8
maurolepore <maurolepore@gmail.com>
2017-04-14 12:53:00 -0500

    Test funs in tutorial growth changes
    * individual_grow.table
    * pospower

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(ctfs)
+
+test_check("ctfs")
---
+++ tests/testthat/test-growth_changes.R
@@ -0,0 +1,32 @@
+context("Functions work in tutorial growth changes")
+
+test_that(
+  "individual_grow.table works with census bci12full1 and bci12full15", {
+    actual <- individual_grow.table(
+      list(
+        bci::bci12full1, 
+        bci::bci12full5
+      )
+    )
+    expect_equal_to_reference(actual, "ref_individual_grow_table.rds")
+    expect_is(actual, "data.frame")
+})
+
+test_that(
+  "pospower works with positive and negative inputs, integer or real", {
+    expected_if_positive <- 4
+    actual <- pospower(2, 2)
+    expect_equal(actual, expected_if_positive)
+    
+    expected_if_negative <- -4
+    actual <- pospower(-2, 2)
+    expect_equal(actual, expected_if_negative)
+    
+    expected_if_positive_real <- 4.84
+    actual <- pospower(2.2, 2)
+    expect_equal(actual, expected_if_positive_real)
+
+    expected_if_negative_real <- -4.84
+    actual <- pospower(-2.2, 2)
+    expect_equal(actual, expected_if_negative_real)
+})
