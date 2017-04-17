1afd60e789bd379a24216f3bc3fdc6594a4eb461
maurolepore <maurolepore@gmail.com>
2017-04-17 15:11:36 -0500

    remove provisional tests

--- tests/testthat/test-population_changes.R
+++ tests/testthat/test-population_changes.R
@@ -1,22 +1,3 @@
 context("functions work in tutorial population changes")
 
-test_that("model.littleR.Gibbs works with bci12full1 and 5 & defaults", {
-  # errs because argument "sptable" is missing, with no default
-  library(date)
-  expect_error({
-    model.littleR.Gibbs(
-      cns1 = bci::bci12full1,
-      cns2 = bci::bci12full5,
-      mindbh = 1
-    )
-  })
-  # solves by providing sptable
-  path <- "../bci16eg/data/bci.spptable.rda"
-  load(path)
-    model.littleR.Gibbs(
-    cns1 = bci::bci12full1,
-    cns2 = bci::bci12full5,
-    sptable = bci.spptable,
-    mindbh = 1
-  )
-})
+
