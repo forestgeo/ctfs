9c74936262cdf13c98151779d6e52f200a138780
maurolepore <maurolepore@gmail.com>
2017-04-17 20:59:50 -0500

    test graph.abundmodel

--- tests/testthat/test-population_changes.R
+++ tests/testthat/test-population_changes.R
@@ -1,3 +1,9 @@
 context("functions work in tutorial population changes")
 
-
+test_that(
+  "graph.abundmodel works with bcixl::mod_little_gibbs", {
+  expect_warning({actual <- graph.abundmodel(bcixl::mod_little_gibbs)})
+  expect_type(actual, "list")
+  expect_length(actual, 2)
+  expect_named(actual, c("Fastest_increases", "Biggest_losses"))
+})
