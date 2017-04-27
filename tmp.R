37cd0d522a27e6a725a9bf4b7f190ba3260a3b0f
maurolepore <maurolepore@gmail.com>
2017-04-27 17:42:24 -0500

    Name new files in R/ as fr-* to indicate this is my addition. Rebuild

--- tmp.R
+++ tmp.R
@@ -10,32 +10,16 @@ load_all()
 
 # tst data ----------------------------------------------------------------
 
-
-## S3 method for class 'data'
-censdata <- bci::bci12full1
-split.data(
-  censdata,
-  splitcol = "sp",
-  keepsplitcol = FALSE,
-  allsplit = NULL,
-  showOutput = NULL
-)
-
-library(stringr)
-read_lines("NAMESPACE") %>% 
-  str_extract("S3method(.*),(.*)$") %>% 
-  as_tibble() %>% 
-  rm_na_row() %>% 
-  mutate(value = str_replace_all(value, "S3method\\((.*),(.*)\\)$", "\\1.\\2")) 
-    
-    %>% 
-  
-  
-  str_replace_all(".*exp.*2par.*$", "export(exp.2par)") %>% 
-  
-  
-  
-  str_replace_all(".*exp.*2par.*$", "export(exp.2par)") %>% 
-
+test_that("split.data outputs the same before and after fix", {
+  censdata <- bci::bci12full1
+  actual <- split.data(
+    censdata,
+    splitcol = "sp",
+    keepsplitcol = FALSE,
+    allsplit = NULL,
+    showOutput = NULL
+    expect_equal_to_reference(actual, "ref_split_data.rds")
+  )
+})
 
 
