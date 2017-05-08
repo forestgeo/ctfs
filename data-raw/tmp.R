c708913c879f6dc62ce920c88aacd37b90f5be67
maurolepore <maurolepore@gmail.com>
2017-05-08 16:17:45 -0500

    update tibble  description

---
+++ data-raw/tmp.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+library(stringr)
+
+load_all()
+
+raw_strings <- raw_strings()
+
+
+
+pull_description <- function(raw_strings) {
+  fun_patt <- raw_strings %>% 
+    stringr::str_extract_all(
+      stringr::regex("^\\'[a-z]+.*$", multiline = TRUE)
+    ) %>% 
+    unlist() %>% 
+    paste0(collapse = "|")
+  
+  raw_strings %>% 
+    stringr::str_split(fun_patt) %>% 
+    tibble() %>% 
+    unnest() %>% 
+    set_names("str") %>% 
+    mutate(
+      str = stringr::str_replace_all(str, stringr::fixed("\r"), ""),
+      str = stringr::str_replace_all(str, stringr::fixed("\r"), ""),
+      str = stringr::str_replace_all(str, stringr::fixed("\n"), ""),
+      str = stringr::str_replace_all(str, stringr::fixed("#"), ""),
+      str = stringr::str_replace_all(str, stringr::fixed("'"), ""),
+      str = stringr::str_replace_all(str, stringr::fixed("@description"), ""),
+      str = stringr::str_trim(str),
+      str = stringr::str_trunc(str, 60)
+    )
+}
+
+tibble_description <- function(raw_strings) {
+  pulled <- pull_description(raw_strings)
+  pulled$fun2match <- stringr::str_extract_all(pulled$str, "^[^ ]*")
+  pulled %>% unnest
+}
+
+
+tibble_description(raw_strings)
