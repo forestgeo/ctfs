1e94898499c83b37bd1d4ba99ab40a071b57ae15
maurolepore <maurolepore@gmail.com>
2017-05-12 14:50:34 -0500

    build  site, but functions starting with uppercase  did not write title

--- data-raw/tmp.R
+++ data-raw/tmp.R
@@ -1,11 +1,22 @@
 entitle_function("./data-raw/to_R/")
 
 # Procedure to build this package
-# - rebuild, document
+# - rebuild package
 build()
+# - document 
+document()
 # - write pkgdown.yml
 write_pkgdown_yml(raw_strings())
 # - test man and pkgdown.yml have the same functions
 test()
 #     - if not, show
 showdiff_man_pkg()
+# build site
+
+
+
+
+raw_strings() %>% 
+  stringr::str_subset("#' AssignDiag\r") %>% 
+  stringr::str_extract("#' AssignDiag\r")
+
