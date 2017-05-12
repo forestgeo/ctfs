ce996a718bbb0e74d57b6e38347ffc00c1f03d8c
maurolepore <maurolepore@gmail.com>
2017-05-12 18:58:33 -0500

    List steps to build  package

--- data-raw/tmp.R
+++ data-raw/tmp.R
@@ -2,15 +2,18 @@
 
 .rs.restartR()
 devtools::build()
-devtools::document()
+# devtools::document()
+
+load_all()
 library(tidyverse)
 write_pkgdown_yml(raw_strings())
 .rs.restartR()
 devtools::test()
+.rs.restartR()
 pkgdown::build_site()
-
 .rs.restartR()
 devtools::check()
+.rs.restartR()
 devtools::install()
 
 # To debug difference in documentation between pkgdown and man
