bfc77c246f22325ed164fce5f9493351672e7b23
maurolepore <maurolepore@gmail.com>
2017-05-12 14:35:37 -0500

    Organize procedure to build package and rewrite _pkgdown

--- data-raw/tmp.R
+++ data-raw/tmp.R
@@ -1,1 +1,11 @@
 entitle_function("./data-raw/to_R/")
+
+# Procedure to build this package
+# - rebuild, document
+build()
+# - write pkgdown.yml
+write_pkgdown_yml(raw_strings())
+# - test man and pkgdown.yml have the same functions
+test()
+#     - if not, show
+showdiff_man_pkg()
