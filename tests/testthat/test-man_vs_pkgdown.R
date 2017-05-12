6c1e5464c68eacf2a220a730b8b746ca67ed0877
maurolepore <maurolepore@gmail.com>
2017-05-12 16:50:36 -0500

    Generalize dir to allow running test  from inside the function or with devtools::test()

--- tests/testthat/test-man_vs_pkgdown.R
+++ tests/testthat/test-man_vs_pkgdown.R
@@ -3,16 +3,23 @@ context("Functions in man/ and _pkgdown.yml are the same")
 test_that("Functions in man/ are all in _pkgdown.yml and viceversa", {
   # Avoid documenting testing internal functions, because they are in man but
   # not indexed
-  exported_not_indexed <- list_internal_funs(raw_strings(path2r = "../../R/"))
+  
+  here <- stringr::str_extract(getwd(), "/[^/]*$")
+  prefix <- ifelse(here == "/forestr" , "./", "../../")
+  here_path <- function(here) {paste0(prefix, here)}
+  
+  exported_not_indexed <- list_internal_funs(
+    raw_strings(path2r = here_path("R/"))
+  )
   exported_not_indexed <- exported_not_indexed$fun_internal
   pkg_doc <- "forestr"
   not_applicable <- c(pkg_doc, exported_not_indexed)
   
-  man <- dir("../../man") %>% 
+  man <- dir(here_path("man")) %>% 
   stringr::str_replace("\\.Rd", "") %>% 
   setdiff(not_applicable)
   
-  pkg <- readr::read_lines("../../_pkgdown.yml") %>% 
+  pkg <- readr::read_lines(here_path("_pkgdown.yml")) %>% 
   stringr::str_subset("^   -") %>% 
   stringr::str_replace("-", "") %>% 
   stringr::str_trim()
