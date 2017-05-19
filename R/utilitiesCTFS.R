a711e6ea6193fed57b6c318806822bdcf9ac6553
maurolepore <maurolepore@gmail.com>
2017-05-19 17:07:43 -0500

    #2 better document assemble.demography.

--- R/utilitiesCTFS.R
+++ R/utilitiesCTFS.R
@@ -69,25 +69,31 @@
 #'
 'countspp'
 
-#' Takes output of a demographic analysis (produced by functions growt...
+
+
+#' Reformat the output from demographic functions from list to dataframe.
 #'
 #' @description
 #'
-#' Takes output of a demographic analysis (produced by functions growth, mortality, or pop.change)
-#' and converts into one dataframe. Only indicated dbh categories are included; be sure that whichdbhcat does 
-#' not exceed the number of columns in the data submitted. Type is 'g'for growth, 'm'
-#' for mortality, 'ba'for basal area, 'agb'for biomass, 'r'for recruitment, and 'a'for abundance.
+#' Takes output of a demographic analysis (produced by functions growth,
+#' mortality, or pop.change) and converts into one dataframe. Only indicated dbh
+#' categories are included; be sure that whichdbhcat does not exceed the number
+#' of columns in the data submitted. Type is 'g'for growth, 'm' for mortality,
+#' 'ba'for basal area, 'agb'for biomass, 'r'for recruitment, and 'a'for
+#' abundance.
 #'
 #' @examples
 #' \dontrun{
 #'
-#' data=pop.change(bci.full5,bci.full6,split1=bci.full5$sp);
-#' result=assemble.demography(data,type='a',whichdbhcat=1)
-#' data=growth(bci.full5,bci.full6,split1=bci.full5$sp)
-#' result=assemble.demography(data,type='g',whichdbhcat=1)
-#' data=mortality.eachspp(bci.full5,bci.full6,classbreak=c(10,100))
-#' result1=assemble.demography(data,type='m',whichdbhcat=1)
-#' result2=assemble.demography(data,type='m',whichdbhcat=2)}
+#' data = pop.change(bci.full5, bci.full6, split1 = bci.full5$sp)
+#' result = assemble.demography(data, type = 'a', whichdbhcat = 1)
+#' data = growth(bci.full5, bci.full6, split1 = bci.full5$sp)
+#' result = assemble.demography(data, type = 'g', whichdbhcat = 1)
+#' data = mortality.eachspp(bci.full5, bci.full6, classbreak = c(10, 100))
+#' result1 = assemble.demography(data, type = 'm', whichdbhcat = 1)
+#' result2 = assemble.demography(data, type = 'm', whichdbhcat = 2)
+#' }
+#' 
 'assemble.demography'
 
 #' This takes a CTFS demography table, output by functions mortality, ...
