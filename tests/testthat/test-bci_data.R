ecdc7ddf2720eced8a82c0edb45810dbc8a8cfae
maurolepore <maurolepore@gmail.com>
2017-08-06 23:24:50 -0400

    Move load packages.

--- tests/testthat/test-bci_data.R
+++ tests/testthat/test-bci_data.R
@@ -1,9 +1,8 @@
 context("bci12 data is complete")
 
-library(dplyr)
-library(bci)
-
 test_that("All census exist in data/", {
+  library(dplyr)
+  library(bci)
   all_censuses <- c(paste0("bci12full", 1:7), paste0("bci12stem", 1:7))
   all_censuses_exist <- lapply(all_censuses, exists) %>% 
     unlist() %>% 
