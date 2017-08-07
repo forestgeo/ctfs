8773a3c72c68b73ae1041b5ad1e74f4105455f52
maurolepore <maurolepore@gmail.com>
2017-08-06 22:57:16 -0400

    Add test to check that all bci 12 data exists.

---
+++ tests/testthat/test-bci_data.R
@@ -0,0 +1,12 @@
+context("bci12 data is complete")
+
+library(dplyr)
+library(bci)
+
+test_that("All census exist in data/", {
+  all_censuses <- c(paste0("bci12full", 1:7), paste0("bci12stem", 1:7))
+  all_censuses_exist <- lapply(all_censuses, exists) %>% 
+    unlist() %>% 
+    all()
+  expect_true(all_censuses_exist)
+})
